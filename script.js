151604e32abaf8a8286ac8f185195ef2cb9cea82
Gestroo <strooge2105@gmail.com>
2021-04-09 15:52:57 +0300

    maket v1.0

---
+++ script.js
@@ -0,0 +1,16 @@
+function knopka() {
+
+        Swal.fire( {
+            allowEscapeKey:'true',
+            background:'#ffe8ff',
+            title:'Внимание!',
+            iconColor:'#c800c8',
+            icon:'info',
+           text:'Макет был реализован Широковым Дмитрием Романовичем. Вариант № 5',
+           timer:'5000',
+           timerProgressBar:'true',
+           confirmButtonColor:'#c800c8',
+           confirmButtonText:'Круто',
+        })
+    
+    } 
